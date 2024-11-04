7422647f6d41b13060a8d5ed5efa0262ad069c4b
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-11-04 19:18:12 +0530

    Deep-js

---
+++ Deep JavaScript Foundation,v3/types/1-Primitive-Types.js
@@ -0,0 +1,46 @@
+// everyone say everything in javascript is an object.
+// but false is not an object ? is's a value of not object
+
+// this is primitive types
+// undefined 
+// String
+// Number
+// Boolean ----- Boolean values are not an object
+// null 
+// BigInt
+// symbol
+// Infinity
+
+// this is non primitive types
+// object
+// Array
+// function
+// class
+
+    // for example
+
+    let a;
+    console.log(typeof a) // undefined not object
+    
+    let b = 20;
+    console.log(typeof b); // Number not object
+    
+    let c = '20';
+    console.log(typeof c); // string not object
+    
+    let d = true;
+    console.log(typeof d); // boolean not object
+    
+    let e = {};
+    console.log(typeof e); // object
+    
+    let f = Symbol();
+    console.log(typeof f) // symbol not object
+    
+    let g = null;
+    console.log(typeof g) // object
+    
+    let h = [1,2,3,4];
+    console.log(typeof h) // object
+      
+    
