e63fbbc0cfc529076980beb94d997f6640ee9c1d
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-11-21 17:41:14 +0530

    first

---
+++ JS/Types/4-Kinds-of-Emptiness.js
@@ -0,0 +1,16 @@
+
+// Kinds of Emptiness => 
+// Undeifiend 
+// Undeclared
+// Unincililezed
+
+// Undeifiend
+let un;
+console.log(un);
+
+// Undeclared
+// console.log(a);
+
+// Uninclized
+b = "mujju";
+console.log(b);
